d1e2fe519d6f4a7b02ec3ea3d6d8e313c0f9bf63
Vivien <vchang103@gmail.com>
2019-01-26 11:18:10 -0800

    add jaswdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	port: 3306,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: "puppers_db"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: process.env.DB_HOST,
+		port: 3306,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: "puppers_db"
+	});
+}
+
 
 connection.connect(function(err) {
 	if (err) {
